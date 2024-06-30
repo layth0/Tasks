191c0698f72832cab471205cd8edbc529b14745e
Pc <Pc@LAYTHSPC>
2024-06-30 05:25:19 +0300

    Add project files.

---
+++ Tasks/Context/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tasks.Models;
+
+namespace Tasks.Context
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options) { }
+
+        public DbSet<Products> Products { get; set; }
+
+    }
+}
---
+++ Tasks/Models/Products.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tasks.Models
+{
+    public class Products
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public string Description { get; set; }
+
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public String Category { get; set; }
+
+
+
+    }
+}
